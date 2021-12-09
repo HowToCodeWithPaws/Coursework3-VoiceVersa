aa074ffdf844f83f363ec56a687f598aaadce6f2
Zubareva Natalia <ndzubareva@edu.hse.ru>
2021-12-09 19:01:19 +0300

    just a nothing really

--- code/app/src/main/java/com/example/voiceversa/AuthActivity.kt
+++ code/app/src/main/java/com/example/voiceversa/AuthActivity.kt
@@ -1,6 +1,9 @@
 package com.example.voiceversa
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class AuthActivity : AppCompatActivity() {
@@ -8,6 +11,25 @@ class AuthActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_auth)
+
+        var login: String = ""
+        var password: String = ""
+
+        val login_text = findViewById<EditText>(R.id.login)
+        val login_password = findViewById<EditText>(R.id.password)
+
+        val log_in_button = findViewById<Button>(R.id.log_in)
+        val sign_in_button = findViewById<Button>(R.id.sign_in)
+
+        log_in_button.setOnClickListener {
+            //TODO проверОчка на сервере
+            val intent = Intent(this, ProcessActivity::class.java)
+            startActivity(intent)
+        }
+
+        sign_in_button.setOnClickListener {
+            //TODO какая-то регистрация
+        }
     }
 
 }
--- code/app/src/main/java/com/example/voiceversa/ProcessActivity.kt
+++ code/app/src/main/java/com/example/voiceversa/ProcessActivity.kt
@@ -1,12 +1,25 @@
 package com.example.voiceversa
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import java.util.*
 
 class ProcessActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_process)
+        Objects.requireNonNull(supportActionBar)!!.hide()
+        val topbar = findViewById<Toolbar>(R.id.top_bar)
+        topbar.setOnMenuItemClickListener { item: MenuItem ->
+            if (item.itemId == R.id.account) {
+                val intent = Intent(this, AccountActivity::class.java)
+                startActivity(intent)
+            }
+            true
+        }
     }
 }
